7485da9bafc728600665737ef6bbe4a168dee279
1jehuang <jeremyhuang55555@gmail.com>
2026-01-24 17:40:05 -0800

    Update to v0.1.1

--- Formula/mmdr.rb
+++ Formula/mmdr.rb
@@ -1,17 +1,17 @@
 class Mmdr < Formula
   desc "Fast Mermaid renderer in Rust (flowchart/class/state/sequence)"
   homepage "https://github.com/1jehuang/mermaid-rs-renderer"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
-    url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.0/mmdr-x86_64-apple-darwin.tar.gz"
-    sha256 "270db0c8a1a70990351720147bc9e7baeb3c22dcf3da172d507dafb2f519e964"
+    url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.1/mmdr-x86_64-apple-darwin.tar.gz"
+    sha256 "d11e2873dffc1462c49e80a7f30b8ae5db2c2a2e3b05161cba6524a49b8dde6a"
   end
 
   on_linux do
-    url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.0/mmdr-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "efcf55baaddf80af8242e92807388acd0c828ebdd5f572487741e0a8b0793e61"
+    url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.1/mmdr-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "00a3bcad49a4c4a423046787e4ca9465c2bb4bd88e6b18a73601cc5b0197c7a5"
   end
 
   def install
