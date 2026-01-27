c56bdecb28ec282ce690a39d2022c8a330916e3a
1jehuang <jeremyhuang55555@gmail.com>
2026-01-27 12:13:22 -0800

    Update mmdr to v0.1.2 with Apple Silicon binary

--- Formula/mmdr.rb
+++ Formula/mmdr.rb
@@ -1,17 +1,23 @@
 class Mmdr < Formula
-  desc "Fast Mermaid renderer in Rust (flowchart/class/state/sequence)"
+  desc "Fast Mermaid diagram renderer in pure Rust (500-1000x faster than mermaid-cli)"
   homepage "https://github.com/1jehuang/mermaid-rs-renderer"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
-    url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.1/mmdr-x86_64-apple-darwin.tar.gz"
-    sha256 "d11e2873dffc1462c49e80a7f30b8ae5db2c2a2e3b05161cba6524a49b8dde6a"
+    on_intel do
+      url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.2/mmdr-x86_64-apple-darwin.tar.gz"
+      sha256 "8fa3f277b8e5f8551e234d92785df52b6afce5453ffae98d65d40b087f3a8dc1"
+    end
+    on_arm do
+      url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.2/mmdr-aarch64-apple-darwin.tar.gz"
+      sha256 "5bbe45f5f197adfc9911703644a8e2d70548abe27bbf29b1ef94218038346f88"
+    end
   end
 
   on_linux do
-    url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.1/mmdr-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "00a3bcad49a4c4a423046787e4ca9465c2bb4bd88e6b18a73601cc5b0197c7a5"
+    url "https://github.com/1jehuang/mermaid-rs-renderer/releases/download/v0.1.2/mmdr-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "d0a4d8fed5438dcdda1c72d3612e2dab838c6afa6da4c7586d255e57ec5363dc"
   end
 
   def install
@@ -20,7 +26,7 @@ class Mmdr < Formula
 
   test do
     input = "flowchart LR\nA-->B\n"
-    system bin/"mmdr", "-e", "svg", "-i", "-", "-o", "out.svg", :input => input
-    assert_predicate testpath/"out.svg", :exist?
+    pipe_output("#{bin}/mmdr -e svg -o test.svg", input)
+    assert_predicate testpath/"test.svg", :exist?
   end
 end
